5932668e7b5523dfbfad595f8aeb90f9357e9d65
Sawyer Cherry <sawyeracherry@gmail.com>
2021-03-15 19:18:37 -0500

    Initial Commit

---
+++ VinDecoder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VinDecoder
+//
+//  Created by Sawyer Cherry on 3/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VinDecoder/VinDecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinDecoderApp.swift
+//  VinDecoder
+//
+//  Created by Sawyer Cherry on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct VinDecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
