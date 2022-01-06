ba87882d735121a6b04947756179e645593b89ed
Sawyer <sawyeracherry@gmail.com>
2022-01-06 09:57:29 -0600

    created basic core data stack

--- VinDecoder/View/VinDecoderApp.swift
+++ VinDecoder/View/VinDecoderApp.swift
@@ -12,10 +12,10 @@ struct VinDecoderApp: App {
     
     @AppStorage("isOnboarding") var isOnboarding: Bool = true
     
+    let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
-            // This tab view displays A way of navigation fot the user, by making a tab bar at the bottom of the screen
-            
             if isOnboarding{
                 OnboardingView()
             } else {
@@ -53,7 +53,76 @@ struct VinDecoderApp: App {
                                 .foregroundColor(Color(UIColor.systemGreen))
                         }.tag(3)
                 }
+                
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
             }
+        }
+    }
+}
+
+
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    
+    let container: NSPersistentContainer
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "CoreDataModel")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+    }
+    
+    func addVehicle(name: String, year: Int, make: String, model: String, engine: String, titleStatus: String, vin: String, driveType: String, transmission: String, trim: String) {
+        withAnimation {
+            let newVehicle = Vehicle(context: container.viewContext)
+            
+            newVehicle.name = name
+            newVehicle.make = make
+            newVehicle.driveType = driveType
+            newVehicle.model = model
+            newVehicle.year = Int16(year)
+            newVehicle.engine = engine
+            newVehicle.trim = trim
+            newVehicle.transmission = transmission
+            newVehicle.titleStatus = titleStatus
+            newVehicle.vin = vin
+            
+            save()
+        }
+        
+    }
+    
+    
+    func addVIN(nickName: String, vin: String) {
+        withAnimation {
+            let newVIN = VIN(context: container.viewContext)
+            
+            newVIN.nickName = nickName
+            newVIN.vin = vin
+            
+            save()
+        }
+    }
+    
+    
+    
+    private func save() {
+        do {
+            try container.viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
     }
 }
